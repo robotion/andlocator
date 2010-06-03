f5cee3a2a21880d7c2247d5d7eee749db132f117
Dirk Jaeckel <dirk.jaeckel@gmail.com>
2010-06-04 00:12:36 +0200

    marker

--- client/Locator/src/com/jaeckel/locator/Map.java
+++ client/Locator/src/com/jaeckel/locator/Map.java
@@ -3,9 +3,11 @@ package com.jaeckel.locator;
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.LinearLayout;
+import android.graphics.drawable.Drawable;
 
-import com.google.android.maps.MapView;
-import com.google.android.maps.MapActivity;
+import com.google.android.maps.*;
+
+import java.util.List;
 
 public class Map extends MapActivity {
 
@@ -13,6 +15,13 @@ public class Map extends MapActivity {
     LinearLayout linearLayout;
     MapView mapView;
 
+    List<Overlay> mapOverlays;
+    Drawable drawable;
+    ItemizedOverlayImpl itemizedOverlay;
+
+
+    GeoPoint point = new GeoPoint(13240000, 53120000);
+    OverlayItem overlayitem = new OverlayItem(point, "", "");
 
     /**
      * Called when the activity is first created.
@@ -20,9 +29,18 @@ public class Map extends MapActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
         setContentView(R.layout.main);
         mapView = (MapView) findViewById(R.id.mapview);
         mapView.setBuiltInZoomControls(true);
+
+        mapOverlays = mapView.getOverlays();
+        drawable = this.getResources().getDrawable(R.drawable.marker);
+        itemizedOverlay = new ItemizedOverlayImpl(drawable);
+
+
+        itemizedOverlay.addOverlay(overlayitem);
+        mapOverlays.add(itemizedOverlay);
     }
 
 
