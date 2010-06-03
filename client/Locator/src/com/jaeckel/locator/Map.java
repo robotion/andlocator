5d8385e9b1900e09519b7b35761f8974701df312
Dirk Jaeckel <dirk.jaeckel@gmail.com>
2010-06-04 01:13:06 +0200

    CCCB

--- client/Locator/src/com/jaeckel/locator/Map.java
+++ client/Locator/src/com/jaeckel/locator/Map.java
@@ -4,10 +4,15 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.widget.LinearLayout;
 import android.graphics.drawable.Drawable;
+import android.graphics.Point;
+import android.location.Geocoder;
+import android.location.Address;
 
 import com.google.android.maps.*;
 
 import java.util.List;
+import java.util.Locale;
+import java.io.IOException;
 
 public class Map extends MapActivity {
 
@@ -20,27 +25,47 @@ public class Map extends MapActivity {
     ItemizedOverlayImpl itemizedOverlay;
 
 
-    GeoPoint point = new GeoPoint(53540000, 13520000);
-    OverlayItem overlayitem = new OverlayItem(point, "", "");
-
     /**
      * Called when the activity is first created.
      */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
+
         setContentView(R.layout.main);
         mapView = (MapView) findViewById(R.id.mapview);
         mapView.setBuiltInZoomControls(true);
 
-        mapOverlays = mapView.getOverlays();
-        drawable = this.getResources().getDrawable(R.drawable.androidmarker);
-        itemizedOverlay = new ItemizedOverlayImpl(drawable);
+        Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+
+        List<Address> locations = null;
+
+        try {
+            locations = geocoder.getFromLocationName("Marienstr. 11, 10117, Berlin", 1);
+
+            Double lon = locations.get(0).getLongitude() * 1E6;
+            Double lat = locations.get(0).getLatitude() * 1E6;
+
+            Projection projection = mapView.getProjection();
 
 
-        itemizedOverlay.addOverlay(overlayitem);
-        mapOverlays.add(itemizedOverlay);
+
+            mapOverlays = mapView.getOverlays();
+            drawable = this.getResources().getDrawable(R.drawable.androidmarker);
+            itemizedOverlay = new ItemizedOverlayImpl(drawable);
+
+            GeoPoint geopoint = new GeoPoint(lat.intValue(), lon.intValue());
+       
+            OverlayItem overlayitem = new OverlayItem(geopoint, "lat: " + lat, "lon: " + lon);
+            System.out.println("lat: " + lat + " lon: " + lon);
+            itemizedOverlay.addOverlay(overlayitem);
+            mapOverlays.add(itemizedOverlay);
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
 
