7dae11a7ca29423b0951d4c586cc0c9056f16e56
Dirk Jaeckel <dirk.jaeckel@gmail.com>
2010-06-03 23:04:04 +0200

    maps api jar

--- client/Locator/src/com/jaeckel/locator/Map.java
+++ client/Locator/src/com/jaeckel/locator/Map.java
@@ -2,14 +2,33 @@ package com.jaeckel.locator;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
-public class Map extends Activity
-{
-    /** Called when the activity is first created. */
+import com.google.android.maps.MapView;
+import com.google.android.maps.MapActivity;
+
+public class Map extends MapActivity {
+
+
+    LinearLayout linearLayout;
+    MapView mapView;
+
+
+    /**
+     * Called when the activity is first created.
+     */
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        mapView = (MapView) findViewById(R.id.mapview);
+        mapView.setBuiltInZoomControls(true);
     }
+
+
+    @Override
+    protected boolean isRouteDisplayed() {
+        return false;
+    }
+
 }
