02ca5638200827d7a542de21340b56b111d4aa7d
Dirk Jaeckel <dirk.jaeckel@gmail.com>
2010-06-05 23:26:52 +0200

    set icon at current position and move map center there

--- client/Locator/src/com/jaeckel/locator/Map.java
+++ client/Locator/src/com/jaeckel/locator/Map.java
@@ -3,10 +3,15 @@ package com.jaeckel.locator;
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 import android.graphics.drawable.Drawable;
 import android.graphics.Point;
 import android.location.Geocoder;
 import android.location.Address;
+import android.location.LocationManager;
+import android.location.Location;
+import android.content.Intent;
+import android.content.Context;
 
 import com.google.android.maps.*;
 
@@ -32,6 +37,8 @@ public class Map extends MapActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+//        startService(new Intent(this, PositioningService.class));
+
         KeyBasedFileProcessor kbfp = new KeyBasedFileProcessor();
 
         kbfp.doIt();
@@ -39,36 +46,6 @@ public class Map extends MapActivity {
         setContentView(R.layout.main);
         mapView = (MapView) findViewById(R.id.mapview);
         mapView.setBuiltInZoomControls(true);
-
-        Geocoder geocoder = new Geocoder(this, Locale.getDefault());
-
-        List<Address> locations = null;
-
-        try {
-            locations = geocoder.getFromLocationName("Marienstr. 11, 10117, Berlin", 1);
-
-            Double lon = locations.get(0).getLongitude() * 1E6;
-            Double lat = locations.get(0).getLatitude() * 1E6;
-
-//            Projection projection = mapView.getProjection();
-
-
-
-            mapOverlays = mapView.getOverlays();
-            drawable = this.getResources().getDrawable(R.drawable.androidmarker);
-            itemizedOverlay = new ItemizedOverlayImpl(drawable);
-
-            GeoPoint geopoint = new GeoPoint(lat.intValue(), lon.intValue());
-       
-            OverlayItem overlayitem = new OverlayItem(geopoint, "lat: " + lat, "lon: " + lon);
-            System.out.println("lat: " + lat + " lon: " + lon);
-            itemizedOverlay.addOverlay(overlayitem);
-            mapOverlays.add(itemizedOverlay);
-
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
 
     }
 
@@ -78,4 +55,58 @@ public class Map extends MapActivity {
         return false;
     }
 
+    public void onResume() {
+        super.onResume();
+
+        LocationManager loc = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+
+        boolean enabledOnly = false;
+        List<String> providers = loc.getProviders(enabledOnly);
+
+        for (String pName : providers) {
+
+
+            if (loc.isProviderEnabled(pName)) {
+                // LocationProvider provider = loc.getProvider(pName);
+
+                Location location = loc.getLastKnownLocation(pName);
+                if (location != null) {
+                    Toast.makeText(this, "Provider " + pName + ": " + location.getLatitude() + "/" + location.getLongitude() +
+                            "{" + location.getAccuracy() + "m}", Toast.LENGTH_LONG).show();
+                    Double lon = location.getLongitude() * 1E6;
+                    Double lat = location.getLatitude() * 1E6;
+
+                    mapOverlays = mapView.getOverlays();
+                    drawable = this.getResources().getDrawable(R.drawable.androidmarker);
+                    itemizedOverlay = new ItemizedOverlayImpl(drawable);
+
+                    GeoPoint geopoint = new GeoPoint(lat.intValue(), lon.intValue());
+
+                    OverlayItem overlayitem = new OverlayItem(geopoint, "lat: " + lat, "lon: " + lon);
+                    Toast.makeText(this, "lat: " + lat + " lon: " + lon, Toast.LENGTH_LONG).show();
+                    itemizedOverlay.addOverlay(overlayitem);
+                    mapOverlays.add(itemizedOverlay);
+
+
+                    MapController controller = mapView.getController();
+
+                    controller.animateTo(geopoint);
+
+                    // use first non null location
+//                    break;
+
+                } else {
+
+                    Toast.makeText(this, "No LastKnownLocation for Provider: " + pName + " available", Toast.LENGTH_LONG).show();
+
+                }
+
+
+            } else {
+                Toast.makeText(this, "LocationProvider: " + pName + " not enabled", Toast.LENGTH_SHORT).show();
+            }
+
+
+        }
+    }
 }
