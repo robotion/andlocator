894124276be4374dffb2fe37b5676e923a6681e0
Dirk Jaeckel <dirk.jaeckel@gmail.com>
2010-06-14 00:02:09 +0200

    for users, you know?

---
+++ service/LocatorService/grails-app/controllers/locatorservice/UserController.groovy
@@ -0,0 +1,35 @@
+package locatorservice
+
+class UserController {
+
+  def index = {
+  }
+
+
+
+  def create = {
+
+
+    if (request.isSecure()) {
+        render("is SSL")
+    } else {
+      render("no SSL")
+
+    }
+  }
+
+  def list = {
+  }
+
+  def get = {
+    // by name
+
+
+  }
+
+  def changePassword = {
+
+
+  }
+
+}
---
+++ service/LocatorService/grails-app/domain/com/jaeckel/locator/User.groovy
@@ -0,0 +1,25 @@
+package com.jaeckel.locator
+
+
+
+import javax.persistence.*
+import com.google.appengine.api.datastore.Text;
+// import com.google.appengine.api.datastore.Key;
+
+@Entity
+class User implements Serializable {
+
+    @Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	Long id
+
+    static constraints = {
+    	id visible:false
+	}
+
+  String name
+  String email
+  String passwordHash
+  Text pubKey
+  
+}
