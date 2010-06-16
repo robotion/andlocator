07e9df4e9c1c25764227780e342c2c25e93917e2
Dirk Jaeckel <dirk.jaeckel@gmail.com>
2010-06-17 01:10:14 +0200

    create_account

--- client/Locator/src/com/jaeckel/locator/user/AccountActivity.java
+++ client/Locator/src/com/jaeckel/locator/user/AccountActivity.java
@@ -2,6 +2,11 @@ package com.jaeckel.locator.user;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.view.View;
+import android.preference.PreferenceManager;
+import android.content.SharedPreferences;
 import com.jaeckel.locator.R;
 
 /**
@@ -15,5 +20,30 @@ public class AccountActivity extends Activity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.account);
+
+        Button save = (Button) findViewById(R.id.save);
+
+        save.setOnClickListener(onSave);
     }
+
+    private View.OnClickListener onSave = new View.OnClickListener() {
+
+        public void onClick(View view) {
+            
+            EditText name = (EditText) findViewById(R.id.name);
+            EditText password = (EditText) findViewById(R.id.password);
+            EditText password_again = (EditText) findViewById(R.id.password_again);
+            EditText email = (EditText) findViewById(R.id.email);
+            EditText pubKey = (EditText) findViewById(R.id.pubKey);
+            EditText secKey = (EditText) findViewById(R.id.secKey);
+            EditText passphrase = (EditText) findViewById(R.id.passphrase);
+
+
+            SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(AccountActivity.this);
+
+
+            prefs.edit();
+
+        }
+    };
 }
